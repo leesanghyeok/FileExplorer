6ca1432aedb65a5eebe2a3ef74677c4a39cb0c72
leesanghyeok <1004xlsrn@gmail.com>
2016-05-15 00:55:32 +0900

    first commit

---
+++ src/school/project/Main.java
@@ -0,0 +1,14 @@
+package school.project;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello world!");
+        JFrame jFrame = new JFrame("hello world");
+        jFrame.setSize(300,300);
+        jFrame.setVisible(true);
+    }
+}

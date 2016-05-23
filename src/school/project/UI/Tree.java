5c92415da4dd98dcf22cc07b12c96c02d1e84d0d
leesanghyeok <1004xlsrn@gmail.com>
2016-05-23 11:03:47 +0900

    c:\ 기준으로 출력하게 했는데, 하위 디렉토리는 아직 미구현이다.

--- src/school/project/UI/Tree.java
+++ src/school/project/UI/Tree.java
@@ -1,24 +1,41 @@
 package school.project.UI;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
+import java.io.File;
 
 /**
  * Created by forhack on 2016-05-23.
  */
 public class Tree extends JTree {
     public Tree() {
-        DefaultMutableTreeNode root = new DefaultMutableTreeNode("root");
-        DefaultMutableTreeNode rrr = new DefaultMutableTreeNode("root");
-        DefaultMutableTreeNode r11r = new DefaultMutableTreeNode("root");
-        DefaultMutableTreeNode rr = new DefaultMutableTreeNode("root");
-        DefaultMutableTreeNode rrr1 = new DefaultMutableTreeNode("root");
-        root.add(rrr);
-        root.add(r11r);
-        rrr.add(rr);
-        rr.add(rrr1);
-        DefaultTreeModel model = new DefaultTreeModel(root);
-        this.setModel(model);
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode();
+        DefaultTreeModel treeModel = new DefaultTreeModel(root);
+
+        File rootFile = new File("C:\\");
+        DefaultMutableTreeNode node = new DefaultMutableTreeNode(rootFile);
+        root.add( node );
+
+        File[] files = rootFile.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                node.add(new DefaultMutableTreeNode(file.getName()));
+            }
+        }
+
+
+        setModel(treeModel);
+        setRootVisible(false);
+        expandRow(0);
+        setVisibleRowCount(15);
+    }
+
+    private File getFileName(File file) {
+        String path = file.getPath();
+        //file.get
+        return null;
+
     }
 }

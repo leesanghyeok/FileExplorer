def18bec3b92953ffda00761fba7282de54d733a
leesanghyeok <1004xlsrn@gmail.com>
2016-05-27 07:55:38 +0900

    처음 로드 될 때 한번에 node 에 삽입하는 방식으로 했다. 하지만 오래걸린다.

--- src/school/project/UI/Tree.java
+++ src/school/project/UI/Tree.java
@@ -1,10 +1,10 @@
 package school.project.UI;
 
 import javax.swing.*;
-import javax.swing.filechooser.FileSystemView;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import java.io.File;
+import java.io.FileFilter;
 
 /**
  * Created by forhack on 2016-05-23.
@@ -16,26 +16,37 @@ public class Tree extends JTree {
 
         File rootFile = new File("C:\\");
         DefaultMutableTreeNode node = new DefaultMutableTreeNode(rootFile);
-        root.add( node );
+        root.add(node);
 
-        File[] files = rootFile.listFiles();
-        for (File file : files) {
-            if (file.isDirectory()) {
-                node.add(new DefaultMutableTreeNode(file.getName()));
-            }
-        }
-
+        File[] files = rootFile.listFiles(directoryFilter);
+        addNodeFile(files,node);
 
         setModel(treeModel);
         setRootVisible(false);
         expandRow(0);
-        setVisibleRowCount(15);
+        //setVisibleRowCount(15);
     }
 
-    private File getFileName(File file) {
-        String path = file.getPath();
-        //file.get
-        return null;
+    private void addNodeFile(File[] file, DefaultMutableTreeNode node) {
+        if (file==null) return;
+        if (file.length == 0) {
+            return ;
+        }
+
+        for (int i=0;i<file.length;i++) {
+            if (file[i].isDirectory()) {
+                DefaultMutableTreeNode inNode = new DefaultMutableTreeNode(file[i].getName());
+                node.add(inNode);
+                addNodeFile(file[i].listFiles(directoryFilter),inNode);
+            }
+        }
 
     }
+
+    private FileFilter directoryFilter = new FileFilter() {
+        @Override
+        public boolean accept(File pathname) {
+            return pathname.isDirectory();
+        }
+    };
 }

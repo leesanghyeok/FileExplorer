c48a882f8b838ff8352e8ce4d947fae0f8fe1abd
leesanghyeok <1004xlsrn@gmail.com>
2016-05-27 08:43:25 +0900

    한번에 모든 node를 넣는게 아니라 클릭시 반응에 따라서 node를 추가하는 식으로 하기로 코드좀 바꿨다.

--- src/school/project/UI/Tree.java
+++ src/school/project/UI/Tree.java
@@ -9,8 +9,10 @@ import java.io.FileFilter;
 /**
  * Created by forhack on 2016-05-23.
  */
-public class Tree extends JTree {
+public class Tree extends JScrollPane {
+    JTree tree;
     public Tree() {
+        tree = new JTree();
         DefaultMutableTreeNode root = new DefaultMutableTreeNode();
         DefaultTreeModel treeModel = new DefaultTreeModel(root);
 
@@ -21,13 +23,35 @@ public class Tree extends JTree {
         File[] files = rootFile.listFiles(directoryFilter);
         addNodeFile(files,node);
 
-        setModel(treeModel);
-        setRootVisible(false);
-        expandRow(0);
+        tree.setModel(treeModel);
+        tree.setRootVisible(false);
+        tree.expandRow(0);
+        setViewportView(tree);
         //setVisibleRowCount(15);
     }
 
+    //하위디렉토리를 node에 넣고, 하위디렉토리 아래에 디렉토리가 있으면 임시 node를 추가해 준다.
+    //나중에 클릭하면 하위디렉토리 출력할 수 있도록
     private void addNodeFile(File[] file, DefaultMutableTreeNode node) {
+        if (file==null) return;
+        if (file.length == 0) return;
+
+        for (int i=0;i<file.length;i++) {
+            if (file[i].listFiles(directoryFilter)==null) continue;
+            if (file[i].isDirectory()) {
+                DefaultMutableTreeNode inNode = new DefaultMutableTreeNode(file[i].getName());
+                node.add(inNode);
+                int len = file[i].listFiles(directoryFilter).length;
+                if (len != 0) {
+                    inNode.add(new DefaultMutableTreeNode());
+                }
+            }
+        }
+    }
+
+
+    //한번에 모든 폴더들을 node에 add하는 재귀함수.
+    private void addAllNodeFile(File[] file, DefaultMutableTreeNode node) {
         if (file==null) return;
         if (file.length == 0) {
             return ;
@@ -37,10 +61,9 @@ public class Tree extends JTree {
             if (file[i].isDirectory()) {
                 DefaultMutableTreeNode inNode = new DefaultMutableTreeNode(file[i].getName());
                 node.add(inNode);
-                addNodeFile(file[i].listFiles(directoryFilter),inNode);
+                addAllNodeFile(file[i].listFiles(directoryFilter),inNode);
             }
         }
-
     }
 
     private FileFilter directoryFilter = new FileFilter() {

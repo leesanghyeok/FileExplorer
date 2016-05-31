1255dbc468c15b2db44da42062cc1ece5cb1fd1d
leesanghyeok <1004xlsrn@gmail.com>
2016-05-31 16:55:32 +0900

    menu 클래스 생성

---
+++ src/school/project/UI/Menu.java
@@ -0,0 +1,17 @@
+package school.project.UI;
+
+import javax.swing.*;
+
+/**
+ * Created by forhack on 2016-05-31.
+ */
+public class Menu extends JMenuBar {
+    JMenu fileMenu;
+    public Menu() {
+        fileMenu = new JMenu();
+        fileMenu.add(new JMenuItem("파일 열기"));
+        fileMenu.add(new JMenuItem("파일 저장"));
+
+        add(fileMenu);
+    }
+}
